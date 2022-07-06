981042bd2dea5f3a607e3e2793cc39b0b6cc19ce
worjs320 <jgkim@sgtechi.co.kr>
2022-07-06 10:10:56 +0900

    Scaffold를 사용한 간단한 앱 구조 만들기 실습

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: Text('안녕')
-      // home: Icon(Icons.star)
-      // home: Image.asset('assets/man.png')
-      // home: Container(width: 50, height: 50, color: Colors.blue,)
-      home: Center(
-        child:  Container(width: 50, height: 50, color: Colors.blue,)
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+        ),
+        body: Text('안녕'),
+        bottomNavigationBar: SizedBox(
+          height: 60,
+          child: BottomAppBar(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ]
+            ),
+          ),
+        )
       )
     );
   }
